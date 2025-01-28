edc1026b6ca751cbaea2e7391d189dcd740c7010
Arshi <arshisabah326@gmail.com>
2025-01-29 03:29:45 +0530

    frontend for smart-agriculture

---
+++ assets/login/app.js
@@ -0,0 +1,13 @@
+// JavaScript placeholder for button click actions
+document.querySelector('.login-btn').addEventListener('click', () => {
+    alert('Login button clicked!');
+  });
+  
+  document.querySelector('.create-btn').addEventListener('click', () => {
+    alert('Create Account button clicked!');
+  });
+  
+  document.querySelector('.microsoft-btn').addEventListener('click', () => {
+    alert('Microsoft Login button clicked!');
+  });
+  
